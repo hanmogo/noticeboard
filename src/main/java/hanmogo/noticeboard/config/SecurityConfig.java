bcd6d194e98bc0b2011d20531d34cab78ce4f520
hanmogo <russell1677@naver.com>
2025-08-10 16:27:24 +0900

    feat: 로그인 기능 구현

--- src/main/java/hanmogo/noticeboard/config/SecurityConfig.java
+++ src/main/java/hanmogo/noticeboard/config/SecurityConfig.java
@@ -29,4 +29,29 @@ public class SecurityConfig {
                 );
         return http.build();
     }
+
+    //로그인, 로그아웃
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                //csrf 보호 비활성화
+                .csrf(csrf -> csrf.disable())
+
+                //HTTP 요청에 대한 접근 권한 설정
+                .authorizeHttpRequests(authorize -> authorize
+                        //지정된 경로에 대해 모든 사용자의 접근을 허용
+                        .requestMatchers("/", "/login", "/join", "/api/posts", "/css/**", "/js/**").permitAll()
+                        //나머지 모든 요청은 인증된 사용자만 접근 허용
+                        .anyRequest().authenticated() //
+                )
+
+                //로그아웃 기능 설정
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
 }
